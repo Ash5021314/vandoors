00d687f23e1f629f49147b125ee57bd5a76ad3cd
unknown <ashkapoghosyan@gmail.com>
2020-02-09 17:48:23 +0400

    fixes slider

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import './About.css'
+import data from '../data.json'
 
 const About = () => {
+  const [about, setAbout] = useState('')
+
+  useEffect(() => {
+    setAbout(data.images.about)
+  }, [])
   return (
     <div className="about">
       <Container>
         <Row>
           <Col>
-            <img src="/images/about/about.jpg" alt=""/>
+            <img key={about} src={about} alt=""/>
           </Col>
           <Col className="flex">
             <h2>О нас</h2>
             <p>VANDOORS - дистрибьютор по продаже входных и межкомнатных дверей.</p>
-            <div className="line"></div>
+            <div className="line"/>
             <p>«Двери Регионов» - торгово-производственная компания, начавшая свою деятельность в 2008 году, сегодня
               является одним из лидирующих поставщиков межкомнатных и входных дверей на московском рынке.</p>
             <p>Целью Группы Компаний «Двери Регионов» является полноценное и комплексное удовлетворение потребности
--- client/src/Components/ControlledCarousel.jsx
+++ client/src/Components/ControlledCarousel.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-
+import React, {useState, useEffect} from 'react'
+import OwlCarousel from 'react-owl-carousel'
+import 'owl.carousel/dist/assets/owl.carousel.css'
+import 'owl.carousel/dist/assets/owl.theme.default.css'
+import data from '../data.json'
 import './ControlledCarousel.css'
 
 const ControlledCarousel = () => {
+  console.log(data)
+  const [slide, setSlide] = useState([])
+  useEffect(() => {
+    setSlide(data.images.slideImages)
+  }, [])
+
   const options = {
     items: 1,
     nav: false,
-    loop:true,
+    loop: true,
     autoplay: true,
     slideBy: 1,
     dots: true,
     dotsEach: true,
     dotData: true,
-    smartSpeed:1500
+    smartSpeed: 1500,
   }
+
   return (
-<div className="slide">
-  <h1 className="carouselHeader">Входные и межкомнатные двери</h1>
-  <OwlCarousel
-    className="owl-theme"
-    {...options}
-  >
-    <div className="item">
-      <img alt="img1" src="/images/slideImages/images.jpg"/>
+    <div className="slide">
+      <h1 className="carouselHeader">Входные и межкомнатные двери</h1>
+      {!slide.length ? <h2>Loading...</h2> : (
+        <OwlCarousel className="owl-theme"  {...options}>
+          {
+            slide.map(res => {
+              return (
+                <div className="item" key={res}>
+                  <img alt="" src={res}/>
+                </div>
+              )
+            })
+          }
+        </OwlCarousel>
+      )}
     </div>
-    <div className="item">
-      <img alt="img1" src="/images/slideImages/blue_house.jpg"/>
-    </div>
-    <div className="item">
-      <img alt="img1" src="/images/slideImages/door-image.jpg"/>
-    </div>
-  </OwlCarousel>
-</div>
   )
 
 }
